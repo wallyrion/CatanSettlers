f3002a0bbc9e72bf08df6754ef300ccdc879a529
Oleksii Korniienko <kornienko1296@gmail.com>
2021-09-08 22:40:21 +0300

    set test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/make/i);
+  expect(linkElement).toBeInTheDocument();
+});
