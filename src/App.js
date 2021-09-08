a5e96cf62a0adcd503a1cdbae00138da0660d68e
Oleksii Korniienko <kornienko1296@gmail.com>
2021-09-08 22:24:14 +0300

    rwer

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 12312312312
+          Learn React 12312312312312312
         </a>
       </header>
     </div>
