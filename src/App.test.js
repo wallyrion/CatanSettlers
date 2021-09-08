ac6f40588fc0e57c90ac6fc5418c84948ddc11bd
Oleksii Korniienko <kornienko1296@gmail.com>
2021-09-08 22:59:54 +0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/make/i);
+  const linkElement = screen.getByText(/Make/i);
   expect(linkElement).toBeInTheDocument();
 });
